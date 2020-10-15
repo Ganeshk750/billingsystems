90c5c5317fc2308e959964c14fe2eb1462dc680b
Ganeshk750 <ganesh9ot@gmail.com>
2020-10-15 09:41:24 +0530

    Adding BaseObject class

--- src/main/java/com/ganesh/api/entity/BaseObject.java
+++ src/main/java/com/ganesh/api/entity/BaseObject.java
@@ -1,13 +1,32 @@
 package com.ganesh.api.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
+
+@Data
+@MappedSuperclass
 public class BaseObject implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
+
+    @JsonIgnore
+    @Column(name="is_deleted")
     private boolean deleted;
+
+    @JsonIgnore
+    @Column(name="date_created")
     private Timestamp dateCreated = new Timestamp(System.currentTimeMillis());
+
+    @JsonIgnore
+    @Column(name="date_modified")
     private Timestamp dateModified;
 
 }
