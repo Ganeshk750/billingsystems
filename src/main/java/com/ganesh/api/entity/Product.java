f6e30d61b157319b14795890b480bfb5f363f29e
Ganeshk750 <ganesh9ot@gmail.com>
2020-10-17 18:21:23 +0530

    # adding product models

--- src/main/java/com/ganesh/api/entity/Product.java
+++ src/main/java/com/ganesh/api/entity/Product.java
@@ -1,4 +1,40 @@
 package com.ganesh.api.entity;
 
-public class Product {
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="product")
+public class Product extends BaseObject {
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(name="description", nullable = false)
+    private String description;
+
+    @Column(name="code", nullable = false)
+    private String code;
+
+    @Column(name="price", nullable = false)
+    private String price;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="tax_id")
+    private Tax tax;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="offer_id")
+    private List<Offer> offers;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="vendor_id")
+    private List<Vendor> vendors;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="category_id")
+    private Category category;
 }
