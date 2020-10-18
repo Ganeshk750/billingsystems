9a77b17a906c9ec1f709e545c453dd3643d470ee
Ganeshk750 <ganesh9ot@gmail.com>
2020-10-18 12:08:56 +0530

    ## adding Test controller

---
+++ src/main/java/com/ganesh/api/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.ganesh.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping
+    public String print() {
+        return "This is test controller";
+    }
+}
