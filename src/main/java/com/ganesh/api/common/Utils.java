9084c7de1e3d41f0327d4f40d44622d88392f84d
Ganeshk750 <ganesh9ot@gmail.com>
2020-10-28 12:15:42 +0530

    #Exception handling and validations

---
+++ src/main/java/com/ganesh/api/common/Utils.java
@@ -0,0 +1,25 @@
+package com.ganesh.api.common;
+
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDateTime;
+
+public class Utils {
+
+    public static String getFullName(String firstName, String middleName, String lastName){
+        String fullName = null;
+        if(StringUtils.isEmpty(firstName) || StringUtils.isEmpty(middleName) || StringUtils.isEmpty(lastName)){
+            return fullName;
+        }
+        if(StringUtils.isEmpty(middleName)){
+            fullName = new StringBuilder().append(firstName).append(" ").append(lastName).toString();
+        }else{
+            fullName = new StringBuilder().append(firstName).append( " ").append(middleName).append(" ").append(lastName).toString();
+        }
+        return  fullName;
+    }
+
+    public static LocalDateTime currentDateTime(){
+        return LocalDateTime.now();
+    }
+}
